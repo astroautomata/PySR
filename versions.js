ab7a13de9f47fe344dcffe6f5083a9e7bd91767f
MilesCranmerBot <miles.cranmer.bot@gmail.com>
2026-08-31 18:11:19 +0100

    Point stable at v2.1.0 and refresh version metadata
    stable/ still redirected to v1.5.9, and versions.js reported 1.5.9 as the
    newest release because only v1.5.9 carried a siteinfo.js.
    
    Co-authored-by: Miles Cranmer <miles.cranmer@gmail.com>

--- versions.js
+++ versions.js
@@ -1,12 +1,11 @@
 var DOC_VERSIONS = [
   "dev",
   "stable",
-  "v1.5.9",
-  "v1",
-  "v1.5",
-  "v2.0.0",
-  "v2.0.0a2",
   "v2.1.0",
+  "v2.0.0",
+  "v1.5.9",
+  "v1.5",
+  "v1",
 ];
-var DOCUMENTER_NEWEST = "1.5.9";
+var DOCUMENTER_NEWEST = "2.1.0";
 var DOCUMENTER_STABLE = "stable";
